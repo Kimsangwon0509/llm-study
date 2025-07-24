4d70270f133675b3d3d70844f836435e7f247a87
Kimsangwon0509 <kimsw509@naver.com>
2025-07-24 13:36:55 +0900

    펑션 콜링 사용해보기

--- chap07/part1/what_time_is_it_terminal.py
+++ chap07/part1/what_time_is_it_terminal.py
@@ -16,3 +16,32 @@ def get_ai_response(messages, tools = None) :
         tools = tools
     )
     return response
+
+messages = [
+    {"role": "system", "content": "너는 사용자를 도와주는 상담사야."},
+]
+
+while True :
+    user_input = input("사용자\t : ")
+    if user_input == "exit":
+        break
+    messages.append({"role": "user", "content": user_input})
+
+    ai_response = get_ai_response(messages, tools=tools)
+    ai_message = ai_response.choices[0].message
+    print(ai_message)
+
+    tool_calls = ai_message.tool_calls
+    if tool_calls :
+        tool_name = tool_calls[0].function.name
+        tool_call_id = tool_calls[0].id
+
+        if tool_name == "get_current_time" :
+            messages.append({"role": "function", "name": tool_name, "content": get_current_time(), "tool_call_id": tool_call_id})
+
+        ai_response = get_ai_response(messages , tools=tools)
+        ai_message = ai_response.choices[0].message
+
+    messages.append(ai_message)
+
+    print("AI\t:" + ai_message.content)
